9512f977305a7252f238b6cf10cfab3180d108ae
Mitchel Smith <mitchel.smith@yahoo.com>
2017-09-28 12:39:11 -0500

    Scrape title, keywords, description. Scrape and sort story.

--- Scraping.js
+++ Scraping.js
@@ -1,23 +1,104 @@
 // These are the variable declarations that are used to scrape the page specified
-var request = require('request'),
-    cheerio = require('cheerio'),
-    urls = [];
+var request = require('request');
+var cheerio = require('cheerio');
+var rawStory = "";
+var all = "";
+var text2 = "";
+var sortedWords;
+var words = [];
+var keyWords = "";
 
 //This specifies the request to have two fields, a string that is used for the Web page to scrape, and a function, specified below
-request('http://www.reddit.com',function(err,resp,body){
+request('https://www.theguardian.com/environment/2017/sep/28/monsanto-banned-from-european-parliament',function(err,resp,body){
     //Basically specified "If retrieved successfully..."
-    if(!err && resp.statusCode == 200){
-        //This uses jQuery format, just loads th4e body of the HTML into this variable
-        var $ = cheerio.load(body);
-        //"For this variable..." Title is the title of pages, "siteTable" (common sense), and for each, perform that function
-        $('a.title', '#siteTable').each(function(){
+if(!err && resp.statusCode == 200){
+    
+    //This uses jQuery format, just loads th4e body of the HTML into this variable
+    var $ = cheerio.load(body);
+    var resObj = {};
 
-            // Grabs the URLs from the "siteTable"
-            var url = $(this).attr('href');
-            // Pushes them onto the previously declared array
-            urls.push(url);
+    //Find everything with a p tag
+    $('p').each(function(index, title){
+
+        // Grab the main story of the article
+        rawStory = $(title).text();
+        rawStory = rawStory.replace(/[^A-Za-z]/g, ' ');
+        rawStory = rawStory.split(' ');
+
+        //Get how many of each word that there is
+        if (rawStory.length) {
+            for (var i = 0, length = rawStory.length; i < length; i += 1) {
+                var matched = false,
+                    word = rawStory[i];
+
+                for (var j = 0, numberOfWords = words.length; j < numberOfWords; j += 1) {
+                    if (words[j][0].toLowerCase() === word.toLowerCase()) {
+                        matched = true;
+                        words[j][1] += 1;
+                    }
+                }
+
+                //Excludes words we don't want and words less than 3 chars long.
+                if (!matched && word.toLowerCase() !== 'this' 
+                             && word.toLowerCase() !== 'will' 
+                             && word.toLowerCase() !== 'most' 
+                             && word.toLowerCase() !== 'also'
+                             && word.toLowerCase() !== ' '
+                             && word.toLowerCase() !== 'the'
+                             && word.toLowerCase() !== 'for'
+                             && word.toLowerCase() !== 'and'
+                             && word.toLowerCase() !== 'too'
+                             && word.toLowerCase() !== 'into'
+                             && word.toLowerCase() !== 'that'
+                             && word.length >= 3) 
+                    words.push([word.toLowerCase(), 1]);
+                }
+    
+                //Sort and get top 10 words
+                words.sort(compareSecondColumn);
+                words = words.slice(0, 10);
+                console.log(words);
+            }
         });
-        // Prints to user the array of URLs
-        console.log(urls);
+    
+        //Find certain things (title, keywords, etc) in the metadata
+        resObj = {};
+        $title = $('head title').text(),
+        $desc = $('meta[name="description"]').attr('content'),
+        $kwd = $('meta[name="keywords"]').attr('content'),
+        $ogTitle = $('meta[property="og:title"]').attr('content'),
+        $ogkeywords = $('meta[property="og:keywords"]').attr('content');
+    
+        if ($title) {
+            resObj.title = $title;
+        }
+    
+        if ($desc) {
+            resObj.description = $desc;
+        }
+    
+        if ($kwd) {
+            resObj.keywords = $kwd;
+        }
+    
+        if ($ogTitle && $ogTitle.length){
+            resObj.ogTitle = $ogTitle;
+        }
+    
+        if ($ogkeywords && $ogkeywords.length){
+            resObj.ogkeywords = $ogkeywords;
+        }
+    
+        console.log(resObj);
     }
 });
+
+
+function compareSecondColumn(b, a) {
+    
+    if (a[1] === b[1])
+        return 0;
+    else
+        return (a[1] < b[1]) ? -1 : 1;
+}
+
